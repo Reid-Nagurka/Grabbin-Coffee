06dada1c096d01c0e66eccc2e2c88b7c89afa34d
Greg Garnhart <gegarnhart@email.wm.edu>
2020-02-20 09:26:20 -0500

    Added most basic and definitely not completed designs for Event Screen

---
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/NewEvent.java
@@ -0,0 +1,19 @@
+package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+ *
+ * This class is used to record user data in terms of events
+ * THEN it uses a Push request to our serverless architecture to schedule the event :)
+ */
+public class NewEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
