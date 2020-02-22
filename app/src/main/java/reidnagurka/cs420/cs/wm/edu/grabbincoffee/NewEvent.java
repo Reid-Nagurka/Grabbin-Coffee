b30f1712a20b0802d7bcc2be606ee41f9320c464
Greg Garnhart <gegarnhart@email.wm.edu>
2020-02-22 13:36:12 -0500

    added an outside-source day-picker. it's nice! it works!
    Also, updated some colors :)

--- app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/NewEvent.java
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/NewEvent.java
@@ -1,8 +1,16 @@
 package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import ca.antonious.materialdaypicker.MaterialDayPicker;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
+
 
 /*
  *
@@ -20,6 +28,29 @@ public class NewEvent extends AppCompatActivity {
     }
 
     private void configureSendInviteButton(){
+        Button sendInviteButton = findViewById(R.id.sendInviteButton);
+
+        // editText fields to get field data
+        final EditText eventNameField = findViewById(R.id.eventTitleField);
+        final EditText eventEmailField = findViewById(R.id.inviteEmailField);
+        final EditText eventLocationField = findViewById(R.id.eventLocationField);
+
+        final MaterialDayPicker materialDayPicker = findViewById(R.id.day_picker);
+
+
+
+
+        sendInviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something with these things!
+                final String eventNameText = eventNameField.getText().toString();
+                final String eventEmailText = eventEmailField.getText().toString();
+                final String eventLocationText = eventLocationField.getText().toString();
+                final List<MaterialDayPicker.Weekday> chosenDays = materialDayPicker.getSelectedDays(); // perhaps we should change this to be a string..
+//                Log.d("Days chosen: ", materialDayPicker.getSelectedDays().toString()); // this works!
+            }
+        });
 
     }
 }
