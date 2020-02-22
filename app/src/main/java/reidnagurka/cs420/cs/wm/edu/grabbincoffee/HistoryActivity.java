fc02361826a75620cfdb2ea04763e2c0615e4957
Greg Garnhart <gegarnhart@email.wm.edu>
2020-02-22 09:41:41 -0500

    Configured main activites to appear when proper button on the menu screen is tapped.

---
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/HistoryActivity.java
@@ -0,0 +1,14 @@
+package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/MainActivity.java
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/MainActivity.java
@@ -3,7 +3,10 @@ package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
 
         //for permissions
         //https://developer.android.com/training/permissions/requesting#java
+
+        // there might be better ways to do this, but i'm going to create methods for each button
+        configureNewEventButton();
+        configureSeeHistoryButton();
+        configureSettingsButton();
+    }
+
+    private void configureNewEventButton(){
+        Button newEventButton = findViewById(R.id.newEventButton);
+        newEventButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewEvent.class));
+            }
+        });
+    }
+
+    private void configureSeeHistoryButton(){
+        Button seeHistoryButton = findViewById(R.id.seeHistoryButton);
+        seeHistoryButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+            }
+        });
+    }
+
+    private void configureSettingsButton(){
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
     }
 }
