b62f12739081e6406fb15a829d3c91532e5b624a
Greg Garnhart <gegarnhart@email.wm.edu>
2020-02-27 21:29:22 -0500

    Testing out the post request stuff, but my android studio is messed up, so i have to delete and repull everything. YIKES

--- app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/SettingsActivity.java
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/SettingsActivity.java
@@ -3,12 +3,81 @@ package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Used to set the dark mode, if you want
+ * ALSO, unlink calendar.
+ *
+ * In this push, greg is using the buttons to make get and post requests to an external API he built
+ */
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
+//        configurePostUpButtion();
     }
+
+//    private void configurePostUpButtion(){
+//        Button postUpButton = findViewById(R.id.postUp);
+//
+//        postUpButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sendPostRequest();
+//            }
+//        });
+//    }
+//
+//    private void sendPostRequest(){
+//        final TextView postTextResponse = findViewById(R.id.postUpTextResponse); // pls delete, just for learning
+//        RequestQueue queue = Volley.newRequestQueue(SettingsActivity.this);
+//        String url = "https://12c213d3.ngrok.io/api/invite";
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                postTextResponse.setText("POST RESPONSE: "+response);
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                postTextResponse.setText("POST ERROR"+error);
+//
+//            }
+//        }){
+//            @Override
+//            protected Map<String, String> getParams(){
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("email","greg.garnhart12@gmail.com");
+//                return params;
+//            }
+//
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError{
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("Content-Type", "application/x-www-form-urlencoded");
+//                return params;
+//
+//            }
+//        };
+//        queue.add(stringRequest);
+//    }
 }
