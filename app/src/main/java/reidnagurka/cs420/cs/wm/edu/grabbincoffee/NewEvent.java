1178d5cdcc4b5596d349d715855aa16069720ebf
Reid-Nagurka <reid@redhoundsoftware.com>
2020-03-03 16:20:21 -0500

    Location fix. Should work now (needed a location request in case none was made beforehand).

--- app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/NewEvent.java
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/NewEvent.java
@@ -1,9 +1,13 @@
 package reidnagurka.cs420.cs.wm.edu.grabbincoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 //import ca.antonious.materialdaypicker.MaterialDayPicker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,6 +27,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -39,6 +46,8 @@ import java.util.Map;
  * THEN it uses a Push request to our serverless architecture to schedule the event :)
  */
 public class NewEvent extends AppCompatActivity {
+
+    //private boolean loc_found_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -233,35 +242,124 @@ public class NewEvent extends AppCompatActivity {
 
     /* Open up webview to Google Maps, let user pick a location, copy location and paste into text field*/
     public void locationHandler(View view) {
-        System.out.println("location handler clicked");
+        //need to check for location permission
         //startActivity(new Intent(NewEvent.this, MapsActivity.class));
-        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        Task locationResult = mFusedLocationProviderClient.getLastLocation();
-        System.out.println(locationResult);
-        mFusedLocationProviderClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        Log.d("Location: ", location.toString());
-                        if (location != null) {
-                            String latitude = Double.toString(location.getLatitude());
-                            String longititude = Double.toString(location.getLongitude());
 
-                            String uri_parse_string = "geo:" + latitude + "," + longititude + "?q=coffee";
-                            Log.d("uri", uri_parse_string);
-                            Uri gmmIntentUri = Uri.parse(uri_parse_string);
-                            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                            mapIntent.setPackage("com.google.android.apps.maps");
-                            if (mapIntent.resolveActivity(getPackageManager()) != null) {
-                                startActivity(mapIntent);
-                            }
+        if (ContextCompat.checkSelfPermission(NewEvent.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Fine location permission Not Granted");
 
-                        }
+            //need to get Permissions
+            //100 is a temporary request code, should be a CONST
+            ActivityCompat.requestPermissions(NewEvent.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    200);
+        }
+        //permission is granted
+        else {
+           // loc_found_check = false;
+            //make a location request so that we get the location and can pass it to launch google maps app
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            LocationCallback locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locRes) {
+                    if (locRes == null) {
+                        //Log.d("Loc", "Location is null in if");
+                        System.out.println("Location is null in if");
+                        return;
+                    } else {
+                        //Log.d("Loc", "Location is not null in else");
+                        System.out.println("Location is not null in else");
+                        //loc_found_check = true;
                     }
-                });
+                }
+            };
+
+            //if (loc_found_check == true) {
+                FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                System.out.println(locationResult);
+                mFusedLocationProviderClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                //  Log.d("Location: ", location.toString());
+                                if (location != null) {
+                                    String latitude = Double.toString(location.getLatitude());
+                                    String longititude = Double.toString(location.getLongitude());
+
+                                    String uri_parse_string = "geo:" + latitude + "," + longititude + "?q=coffee";
+                                    Log.d("uri", uri_parse_string);
+                                    Uri gmmIntentUri = Uri.parse(uri_parse_string);
+                                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                    mapIntent.setPackage("com.google.android.apps.maps");
+                                    if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                                        startActivity(mapIntent);
+                                    }
+
+                                }
+                            }
+                        });
+       //   }
+        }
 
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        //more cases for more permissions, for right now 100 is just a placeholder, should be switched to CONST
+        switch (requestCode) {
+            case 200: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    System.out.println("Permission was granted, going to new screen from override");
+                                        //startActivity(new Intent(NewEvent.this, MapsActivity.class));
+                    FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                    Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                    System.out.println(locationResult);
+                    mFusedLocationProviderClient.getLastLocation()
+                            .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                                @Override
+                                public void onSuccess(Location location) {
+                                    // Got last known location. In some rare situations this can be null.
+
+                                    if (location != null) {
+                                        Log.d("Location: ", location.toString());
+                                        String latitude = Double.toString(location.getLatitude());
+                                        String longititude = Double.toString(location.getLongitude());
+
+                                        String uri_parse_string = "geo:" + latitude + "," + longititude + "?q=coffee";
+                                        Log.d("uri", uri_parse_string);
+                                        Uri gmmIntentUri = Uri.parse(uri_parse_string);
+                                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                        mapIntent.setPackage("com.google.android.apps.maps");
+                                        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                                            startActivity(mapIntent);
+                                        }
+
+                                    }
+                                }
+                            });
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    System.out.println("User denied permission");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
