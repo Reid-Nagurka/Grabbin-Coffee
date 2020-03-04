2210ce3ab59d70fbddfc3c3bd4e189cbb2ddf681
Reid-Nagurka <reid@redhoundsoftware.com>
2020-03-04 00:57:45 -0500

    File reading and writing. Did not use encryption, just the private app storage (only storing user email address). Next step is to read from the file where it will actually be used (for proof of concept the file is just read back immediately).

--- app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/SettingsActivity.java
+++ app/src/main/java/reidnagurka/cs420/cs/wm/edu/grabbincoffee/SettingsActivity.java
@@ -6,17 +6,30 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -63,6 +76,8 @@ public class SettingsActivity extends AppCompatActivity {
 
 
         configurePostUpButtion();
+
+        emailAddressHandler();
     }
 
     private void putContentValues(){
@@ -93,6 +108,65 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void emailAddressHandler() {
+        final EditText email_input = findViewById(R.id.userEmailField);
+
+        email_input.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                //runs once user has clicked submit key on keyboard
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                    //gets byte array from email field
+                    byte[] bytes = email_input.getText().toString().getBytes();
+//                    for(int i = 0; i < bytes.length; i++) {
+//                        System.out.println("byte: " + bytes[i]);
+//                    }
+//                    String s = new String(bytes);
+//                    System.out.println(s);
+
+                    //filename used for storage
+                    String filename = "coffee_app_storage.txt";
+
+                    //gets file in the private app storage w filename above and writes to it
+                    try (FileOutputStream fos = openFileOutput(filename, MODE_PRIVATE)) {
+                        fos.write(bytes);
+                        
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        System.out.println("File not found fos");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //reading back, will go elsewhere but proof of working code here
+                    try {
+                        FileInputStream fis = openFileInput(filename);
+                        //checks to make sure there is data to be read
+                        if (fis.available() > 0) {
+                            //reads in bytes from file to a byte array
+                            byte[] read_bytes = new byte[fis.available()];
+                            fis.read(read_bytes);
+                            //converts back byte array to string
+                            String email_as_string = new String(bytes);
+                            System.out.println("Reading back final: " + email_as_string);
+                        }
+                    }
+                    catch (FileNotFoundException e) {
+                        System.out.println("File not found fis");
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
 
